e48bba295c96b59f9b39d2b7b608e4e3bc8ea2ef
vitaminjeff <125537+vitaminjeff@users.noreply.github.com>
2022-07-15 10:49:48 -0700

    Add initial fsharp console files

---
+++ Program.fs
@@ -0,0 +1,13 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+// printfn "Implicit Hello World from F#"
+
+// Learn more about F# at http://fsharp.org
+
+open System // like using in C#
+
+[<EntryPoint>] // attributes use compound brackets [<>], tells .net where to start
+let main argv =
+    printfn "Explicit Hello World from F#!" // print formatted w/ newline
+    0 // return an integer exit code
+
+// let declares or "binds" a value or function w/ a name
