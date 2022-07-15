1e9baae6bf47b7915c16976450a754eb32415fb2
vitaminjeff <125537+vitaminjeff@users.noreply.github.com>
2022-07-15 15:06:33 -0700

    Update directory structure, fix launch.json and tasks.json for debugging multiple projects in vs code

---
+++ studentscores/Program.fs
@@ -0,0 +1,32 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+// printfn "Hello from F#"
+open System
+open System.IO
+
+let printMeanScore (row : string) =
+   let elements = row.Split('\t')
+   printfn "%s" row
+
+let summarize filePath =
+   let rows = File.ReadAllLines filePath
+   let studentCount = (rows |> Array.length) - 1
+   printfn "Student count: %i" studentCount
+   rows
+   |> Array.skip 1
+   |> Array.iter printMeanScore
+
+[<EntryPoint>]
+let main argv =
+   if argv.Length = 1 then
+      let filePath = argv.[0]
+      if File.Exists filePath then
+         printfn "Processing %s" filePath
+         summarize filePath
+         0
+      else
+         printfn "File not found: %s" filePath
+         2
+   else
+      printfn "Please specify a file"
+      1
+
