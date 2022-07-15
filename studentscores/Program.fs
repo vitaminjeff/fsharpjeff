758765065e08d8940d8a36585fede30ad3b380c3
vitaminjeff <125537+vitaminjeff@users.noreply.github.com>
2022-07-15 15:28:26 -0700

    Change output to display mean, min, and max

--- studentscores/Program.fs
+++ studentscores/Program.fs
@@ -7,13 +7,14 @@ let printMeanScore (row : string) =
    let elements = row.Split('\t')
    let name = elements.[0]
    let id = elements.[1]
-   let meanScore =
+   let scores =
       elements
       |> Array.skip 2
-      // |> Array.map float
-      // |> Array.average
-      |> Array.averageBy float
-   printfn "%s\t%s\t%0.1f" name id meanScore
+      |> Array.map float
+   let meanScore = scores |> Array.average
+   let minScore = scores |> Array.min
+   let maxScore = scores |> Array.max
+   printfn "%s\t%s\t%0.1f\t%0.1f\t%0.1f" name id meanScore minScore maxScore
 
 let summarize filePath =
    let rows = File.ReadAllLines filePath
