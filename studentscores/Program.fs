b7fd047520120599a8241cd4b71987ca28290d19
vitaminjeff <125537+vitaminjeff@users.noreply.github.com>
2022-07-15 15:18:48 -0700

    Calculate average of scores

--- studentscores/Program.fs
+++ studentscores/Program.fs
@@ -5,7 +5,14 @@ open System.IO
 
 let printMeanScore (row : string) =
    let elements = row.Split('\t')
-   printfn "%s" row
+   let name = elements.[0]
+   let id = elements.[1]
+   let meanScore =
+      elements
+      |> Array.skip 2
+      |> Array.map float
+      |> Array.average
+   printfn "%s\t%s\t%0.1f" name id meanScore
 
 let summarize filePath =
    let rows = File.ReadAllLines filePath
