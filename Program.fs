fbabce5fbdf788cb3ba24ba8551836e8909b5ddf
vitaminjeff <125537+vitaminjeff@users.noreply.github.com>
2022-07-15 13:44:26 -0700

    Motivational transparency is a fancy term

--- Program.fs
+++ Program.fs
@@ -7,8 +7,25 @@ open System // like using in C#
 
 [<EntryPoint>] // attributes use compound brackets [<>], tells .net where to start
 let main argv =
-    printfn "Explicit Hello World from F#!" // print formatted w/ newline
-    printfn "The args are: %A" argv // print formatted w/ newline
+    // let person = argv.[0]
+    // printfn "Hello World from F#!"
+
+    // let mutable person = "Anonymous Person"
+    // if argv.Length > 0 then
+    //     person <- argv.[0]
+
+    // minimize the use of mutable values
+    let person = 
+        if argv.Length > 0 then
+            argv.[0]
+        else
+            "Anonymous Person"
+
+    printfn "Hello %s from F#!" person
+    // printfn "Explicit Hello World from F#!" // print formatted w/ newline
+    // printfn "The args are: %A" argv // print formatted w/ newline
     0 // return an integer exit code
 
 // let declares or "binds" a value or function w/ a name
+
+// motivational transparency
---
+++ README.md
@@ -0,0 +1,23 @@
+# F# Practice
+
+Writing some practice F# console code! 
+
+Thanks 🔥🔥🔥 to [@kitlovesfsharp](https://twitter.com/kitlovesfsharp) and his Udemy course [F# From the Ground Up](https://www.udemy.com/course/fsharp-from-the-ground-up).
+
+# Running in VS Code
+
+## dotnet run
+```
+dotnet run
+```
+
+## dotnet watch
+```
+dotnet watch run
+```
+
+## Ionide Debug
+Use the `Ionide for F#` VS Code extension.
+
+## VS Code Debug
+You need to set up launch.json and tasks.json.
